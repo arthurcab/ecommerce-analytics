d607f4e0b438f97f4eea7d773b9f3df8634ee9a9
arthurcab <arthur.cmcabral@gmail.com>
2024-09-15 21:18:35 -0300

    Generate customer and sales data with additional fields
    - Add new fields to the generated customer data, including preferred device, preferred payment methods, and gender.
    - Update the registration date range for more recent data.
    - Generate sales data with random product IDs, quantities, prices, and dates
    
    - Different churn patterns are WIP

--- generate_data.py
+++ generate_data.py
@@ -1,4 +1,3 @@
-
 """
 This script generates fake customer data using the Faker library and saves it to a CSV file.
 
@@ -12,8 +11,10 @@ The generated customer data includes the following fields:
 - street_address: Street address of the customer.
 - city: City of the customer.
 - state: State of the customer.
-- country: Country of the customer (always 'USA').
+- prefered_device: Preferred device of the customer (Mobile, Desktop, or Tablet).
 - email_opt_in: Boolean value indicating whether the customer has opted in for email communication.
+- prefered_payment_methods: Preferred payment method of the customer (Credit Card, Paypal, Apple Pay, or Google Pay).
+- gender: Gender of the customer (male or female).
 
 Note: The Faker library is used to generate realistic fake data for testing and demonstration purposes.
 """
@@ -21,6 +22,7 @@ Note: The Faker library is used to generate realistic fake data for testing and 
 # Imports
 import pandas as pd
 from faker import Faker
+import numpy as np 
 
 # Create a Faker object
 fake = Faker(use_weighting=True, locale='en_US', include_private=False)
@@ -36,21 +38,28 @@ for _ in range(10000):
         'name': fake.first_name(),
         'last_name': fake.last_name(),
         'email': fake.email(),
-        'registration_date': fake.date_between(start_date='-1y', end_date='today'),
+        'registration_date': fake.date_between(start_date='-5y', end_date='today'),
         'birth_date': fake.date_of_birth(minimum_age=18, maximum_age=65),
         'street_address': fake.street_address(),
         'city': fake.city(),
         'state': fake.state(),
-        'country': 'USA',
-        'email_opt_in': fake.boolean(chance_of_getting_true=40)
-    })
+        'prefered_device': fake.random_element(elements=('Mobile', 'Desktop', 'Tablet')),
+        'email_opt_in': fake.boolean(chance_of_getting_true=40),
+        # Make 'Credit Card' the most common payment method
+        'prefered_payment_methods': fake.random_element(elements=('Credit Card' ,'Credit Card', 'Credit Card', 'Paypal', 'Apple Pay', 'Google Pay')),
+        'gender': fake.random('male', 'female'),
+        })
+
+# Save the data to a DataFrame
+customers_df = pd.DataFrame(customers)
+
+# Generate sales data
 
 
 if __name__ == '__main__':
     print('Customer data has been generated and saved to data/customers.csv')
 
-    # Save the data to a DataFrame
-    customers_df = pd.DataFrame(customers)
+
 
     # Export the csv file 
     customers_df.to_csv('data/customers.csv', index=False)
